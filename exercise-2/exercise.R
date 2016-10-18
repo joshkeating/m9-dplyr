dc6f91acbdf908c2e31c7f105e171afbd494c602
Josh Keating <josh.keating7@gmail.com>
2016-10-18 14:52:48 -0700

    get that dplyr

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -2,25 +2,23 @@
 # Use a different appraoch to accomplish the same tasks as exercise-1
 
 # Install devtools package: allows installations from GitHub
-install.packages('devtools')
 
 # Install "fueleconomy" package from GitHub
-devtools::install_github("hadley/fueleconomy")
 
 # Require/library the fueleconomy package
-
+require('fueleconomy', 'dplyr')
 
 # You should have have access to the `vehicles` data.frame
-
+data.frame(vehicles)
 
 # Create a data.frame of vehicles from 1997
-
+vehicles.1997 <- vehicles[vehicles$year == 1997,]
 
 # Use the `unique` function to verify that there is only 1 value in the `year` column of your new data.frame
-
+check.unique <- unique(vehicles.1997$year)
 
 # Create a data.frame of 2-Wheel Drive vehicles that get more than 20 miles/gallon in the city
-
+two.wheel.morethan20 <- select(vehicles, drive == '2-Wheel Drive', cty > 20)
 
 # Of those vehicles, what is the vehicle ID of the vehicle with the worst hwy mpg?
 
